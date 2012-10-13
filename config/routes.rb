3969a8c095080b740b7be60a6c8008ecb9dbd5d5
Tomoya Chiba <tomo.asleep@gmail.com>
2012-10-14 01:42:57 +0900

    Create Reports model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Kadaibot::Application.routes.draw do
+  resources :reports
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +51,7 @@ Kadaibot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root to: 'reports#index'
 
   # See how all your routes lay out with "rake routes"
 
