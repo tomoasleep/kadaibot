e251b08cd11653fe2864088020543c1d0000c4a6
Tomoya Chiba <tomo.asleep@gmail.com>
2012-10-14 01:11:56 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kadaibot::Application.config.secret_token = 'ce7a59a8276027689e3d37d9bbb1c112bc5f604df95c8a197ec6517a01dbbe919cd80774ee37a41d7bb73a57e931ba5095420eb658dff55e3b0b592af0254898'
