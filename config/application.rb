7786e086ede6c89b30733bdc705c2040f74c87bb
Tomoya Chiba <tomo.asleep@gmail.com>
2012-10-14 20:30:10 +0900

    bugfix

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,7 @@ module Kadaibot
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initializer_on_precompile = false
   end
 end
 
