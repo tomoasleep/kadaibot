66c6dd2d083cc396ccc5656ed49704aa5cfd59b9
Tomoya Chiba <tomo.asleep@gmail.com>
2012-10-14 19:17:54 +0900

    bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,10 @@ Kadaibot::Application.routes.draw do
   resources :reports
 
   devise_for :admins, only: [:session] do
-    namespace :admins do
-      get '/sign_in', :to => 'devise/sessions#new', :as => :new_admin_session
-      get "/sign_out", to: "devise/sessions#destroy", as: "destroy_admin_session"
-      #get '/sign_up', :to => 'devise/registrations#new', :as => :new_admin_registration
+    get '/sign_in', :to => 'devise/sessions#new', :as => :new_admin_session
+    get "/sign_out", to: "devise/sessions#destroy", as: "destroy_admin_session"
+    #get '/sign_up', :to => 'devise/registrations#new', :as => :new_admin_registration
       #post '/', :to => 'devise/registrations#create', :as => :admin_registration
-    end
   end
   # The priority is based upon order of creation:
   # first created -> highest priority.
